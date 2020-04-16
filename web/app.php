f8ceb91814a5e34380a355d0e77567cb5ce7007b
ted <lambert.tedaldi@gmail.com>
2020-04-16 03:54:03 +0200

    Update layout.html.twig and app.php

--- web/app.php
+++ web/app.php
@@ -8,10 +8,10 @@ include_once __DIR__.'/../var/bootstrap.php.cache';
 
 $kernel = new AppKernel('prod', true);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
+Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 Request::setTrustedProxies(array($request->server->get('REMOTE_ADDR')));
 Request::setTrustedHeaderName(Request::HEADER_FORWARDED, null);
