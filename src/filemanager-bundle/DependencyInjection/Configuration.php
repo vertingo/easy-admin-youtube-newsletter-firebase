3eb80ae44030b9ecd51d366d9b81158a687a7a5a
ted <lambert.tedaldi@gmail.com>
2020-04-17 06:33:21 +0200

    Update ManagerController.php, Configuration.php, and FileManagerExtension.php

--- src/filemanager-bundle/DependencyInjection/Configuration.php
+++ src/filemanager-bundle/DependencyInjection/Configuration.php
@@ -16,7 +16,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('artgris_file_manager');
+        $rootNode = $treeBuilder->root('file_manager');
 
         $rootNode
             ->children()
